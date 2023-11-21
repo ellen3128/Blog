df2ec3f3ef5bd11c8b2adefa46a74ff7c30743f2
ellen3128 <hyunpark.ellen@gmail.com>
2023-11-21 02:24:01 -0500

    completes login and register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,8 +6,9 @@ const Login = () => {
     <div className='auth'>
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input required type="text" placeholder="username" />
+        <input required type="email" placeholder="email" />
+        <input required type="password" placeholder="password" />
         <button>Login</button>
         <p>This is an error!</p>
         <span> Don't have an account? <Link to="/register">Register</Link></span>
