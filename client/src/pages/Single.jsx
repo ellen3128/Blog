dec3e803a98afbe0629ae91a4095e37b63580d07
ellen3128 <hyunpark.ellen@gmail.com>
2023-11-22 12:28:49 -0500

    parses initial layout for single page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Edit from '../img/edit.png';
+import Delete from '../img/delete.png';
+
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className="content">
+        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+      <div className="user">
+        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+      <div className="info">
+        <span>Ellen</span>
+        <p>Posted 2 days ago</p>
+      </div>
+      <div className="edit">
+        <Link to={`/write?edit=2`}>
+        <img src={Edit} alt="" />
+        </Link>
+        <img src={Delete} alt="" />
+      </div>
+      </div>
+      </div>
+      <div className="menu">m</div>
+    </div>
   )
 }
 
