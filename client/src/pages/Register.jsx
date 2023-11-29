966ad5072dfb07e4f1473952a453b3b43b05b3b7
ellen3128 <hyunpark.ellen@gmail.com>
2023-11-29 16:46:17 -0500

    installs axios and modifies package.json proxy

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username:"",
+    email: "",
+    password: "",
+  }) 
+
+  const handleChange = e => {
+    setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  // console.log(inputs)
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const res = await axios.post("")
+  }
+
   return (
     <div className='auth'>
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Register</button>
+        <input required type="text" placeholder="username" name="username" onChange={handleChange}/>
+        <input required type="email" placeholder="email" name="email" onChange={handleChange} />
+        <input required type="password" placeholder="password" name="password" onChange={handleChange}/>
+        <button onClick={handleSubmit}>Register</button>
         <p>This is an error!</p>
         <span> Already registered? <Link to="/login">Login</Link></span>
       </form>
